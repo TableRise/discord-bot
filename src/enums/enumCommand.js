2d47751f0318c36a13c553750fe8bb774732cf87
Ash Trindade <ashtrindade5@gmail.com>
2023-09-02 15:14:49 -0300

    fix: add enumCommand

---
+++ src/enums/enumCommand.js
@@ -0,0 +1,13 @@
+const commandId = {
+  BUG: '1122311300027011077',
+  SUGGESTION: '1122311300027011076',
+  SUPPORT: '1122311300027011078'
+}
+
+const commandMention = {
+  BUG: '</bug:1122311300027011077>',
+  SUGGESTION: '</sugestao:1122311300027011076>',
+  SUPPORT: '</support:1122311300027011078>'
+}
+
+module.exports = { commandId, commandMention }
